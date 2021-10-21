cd71db8d4ec1a282acf7967d02cacc568af27230
Sangyub <godtttever@naver.com>
2021-10-22 01:13:39 +0900

    #3.3

--- src/App.js
+++ src/App.js
@@ -1,40 +1,107 @@
 import React from 'react';
+import axios from 'axios';
+import PropTypes from "prop-types"
 
-function Food({name,picture}){
-  return <div>
-      <h2>i like {name}</h2>
-      <img src={picture} />
-    </div>
-}
+// function Food({name,picture,rating}){
+//   return <div>
+//       <h2>i like {name}</h2>
+//       <h2>{rating}/5.0</h2>
+//       <img src={picture} alt={name}/>
+//     </div>
+// }
 
-const foodILike = [
-  {
-  name: "Kimchi",
-  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
-  },
-  {
-  name: "bulgogi",
-  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM.jpg"
-  },
-  {
-  name: "kimbap",
-  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM.jpg"
-  },
-  {
-  name: "samgyetang",
-  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+// Food.propTypes={ //내가 전달받은 props가 정확한지 내가 원하는 게 맞는지 확인해준다!
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired
+// }
+
+// const foodILike = [
+  
+//   {
+//   name: "Kimchi",
+//   image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+//   rating: 5
+//   },
+//   {
+//   name: "bulgogi",
+//   image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM.jpg",
+//   rating: 4.9
+//   },
+//   {
+//   name: "kimbap",
+//   image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM.jpg",
+//   rating: 3.2
+//   },
+//   {
+//   name: "samgyetang",
+//   image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+//   rating: 2.8
+//   }
+// ];
+
+// function App() {
+//   return (
+//     <div>
+//       {foodILike.map(dish => (
+//       <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+//       ))}
+//     </div>
+    
+//   );
+// }
+
+// class App extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //   console.log("im king")
+  // }
+  // state = { //데이터가 변해서 사용함. 중요함
+  //   count: 0
+  // }
+  // add = () => { //매순간 setState를 호출할 때마다 react는 새로운 state와 render를 호출한다.
+  //   this.setState(current => ({count:current.count+1}))
+  // }
+  // minus = () => {
+  //   this.setState(current => ({count:current.count-1}))
+  // }
+  // componentDidMount(){
+  //   console.log("king of world")
+  // }
+
+  // componentDidUpdate(){
+  //   console.log("just kidding")
+  // }
+
+  // componentWillUnmount(){
+  //   console.log("잔인한놈")
+  // }
+
+  // render(){ //컴포넌트의 데이터를 바꾸기위한 작업임
+  //   //class 문법이니까 당연히 this를 써줘야한다.
+  //   console.log("rendering operation")
+//     return <div>
+//         <h1>The number is: {this.state.count}</h1>; 
+//         <button onClick={this.add}>Add</button>
+//         <button onClick={this.minus}>minus</button>
+//       </div>
+//   }
+// }
+
+class App extends React.Component{
+  state = {
+    isLoading: true
   }
-];
-
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading: false})
+    },6000)
+  }
+  render(){
+      const{isLoading} = this.state
+      return <div>{isLoading ? "isLoading..." : "we are ready" } </div>
+  }
     
-    </div>
-    
-  );
 }
 
 export default App;

538e28c8b5875fd65ba448d8ed75866e5449dc08
Sangyub <godtttever@naver.com>
2021-10-22 02:01:55 +0900

    4.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 import PropTypes from "prop-types"
 
 // function Food({name,picture,rating}){
@@ -94,17 +95,32 @@ class App extends React.Component{
   }
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    this.setState({movies, isLoading: false})
   }
 
   componentDidMount(){
     this.getMovies()
   }
   render(){
-      const{isLoading} = this.state
-      return <div>{isLoading ? "isLoading..." : "we are ready" } </div>
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />    
+              ))}
+              </div>
+            );
   }
-    
 }
 
 export default App;
